d631ee8ac894742b8694190c8306cbcca8acc818
Babek Baratov <mamashaniko@gmail.com>
2024-04-09 15:43:30 +0200

    Update: 0.34

---
+++ WarehouseLabelPrintingSystem/View/ExportToFileWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WarehouseLabelPrintingSystem.Model;
+using WarehouseLabelPrintingSystem.Utilities;
+using WarehouseLabelPrintingSystem.ViewModel;
+
+namespace WarehouseLabelPrintingSystem.View
+{
+    public partial class ExportToFileWindow : Window
+    {
+        private readonly Product _product;
+
+        public ExportToFileWindow(Product product)
+        {
+            InitializeComponent();
+
+            _product = product;
+
+            LoadProductData();
+        }
+
+        private void LoadProductData()
+        {
+            if(_product != null )
+            {
+                Product_Id_Text.Text = $"Id: {_product.product_id}";
+                Product_Number_Text.Text = $"Number: {_product.product_number}";
+                Product_Barcode_Text.Text = $"Barcode: {_product.barcode}";
+                Product_Unit_Text.Text = $"Unit: {_product.unit}";
+                Product_Name_Text.Text = $"Name: {_product.product_name}";
+
+                List<GridViewColumn> columns = CreateCustomFieldColumns();
+
+                GridView_CustomField.Columns.Clear();
+
+                foreach (var item in columns)
+                {
+                    GridView_CustomField.Columns.Add(item);
+                }
+
+                ListView_CustomFields.ItemsSource = _product.custom_fields;
+            }
+        }
+
+        private void Save_to_File_Click(object sender, RoutedEventArgs e)
+        {
+            CustomField customField = (CustomField)ListView_CustomFields.SelectedItem;
+
+            if(customField != null )
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf";
+
+                if(saveFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    BarcodeGenerationAndSavingToPDF(filePath);
+                }
+            }
+        }
+
+        private void ListView_CustomFields_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListView_CustomFields.SelectedItem != null)
+            {
+                Save_to_File.IsEnabled = true;
+            }
+        }
+
+        private void ListView_CustomFields_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private List<GridViewColumn> CreateCustomFieldColumns()
+        {
+            string[] desiredProperties = new string[] {
+                "id",
+                "label",
+                "value",
+            };
+
+            List<GridViewColumn> columns = new();
+
+            foreach (var propertyName in desiredProperties)
+            {
+                PropertyInfo? property = typeof(CustomField).GetProperty(propertyName);
+
+                if (property != null)
+                {
+                    GridViewColumn column = new()
+                    {
+                        Header = property.Name,
+                        DisplayMemberBinding = new Binding(property.Name)
+                    };
+                    columns.Add(column);
+                }
+            }
+
+            return columns;
+        }
+
+        private void BarcodeGenerationAndSavingToPDF(string filePath)
+        {
+            CustomField customField = (CustomField)ListView_CustomFields.SelectedItem;
+
+            LabelViewModel label = new()
+            {
+                ProductNumber = _product.product_number,
+                ProductName = _product.product_name,
+                Unit = _product.unit,
+                Location = customField.value,
+                BarcodeText = BarcodeFormatNumber.FormatNumber(_product.barcode!),
+                ProductNumberPosition = new PointF(11f, 0f),
+                UnitPosition = new PointF(160f, -18f),
+                ProductNamePosition = new PointF(10f, 8f),
+                LocationPosition = new PointF(10f, 6f),
+                BarcodePosition = new PointF(0f, 15f),
+                BarcodeTextPosition = new PointF(6f, 25f),
+                NotePosition = new PointF(10f, 0f)
+            };
+
+            label.GenerateLabel(filePath, _product.barcode!);
+        }
+    }
+}
---
+++ WarehouseLabelPrintingSystem/ViewModel/LabelViewModel.cs
@@ -0,0 +1,74 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.Drawing;
+using System.IO;
+using ZXing.Common;
+using ZXing;
+using ZXing.Windows.Compatibility;
+
+namespace WarehouseLabelPrintingSystem.ViewModel
+{
+    public class LabelViewModel
+    {
+        public string? ProductNumber { get; set; }
+        public string? ProductName { get; set; }
+        public string? Unit { get; set; }
+        public string? Location { get; set; }
+        public string? BarcodeText { get; set; }
+        public PointF BarcodePosition { get; set; }
+        public PointF ProductNumberPosition { get; set; }
+        public PointF ProductNamePosition { get; set; }
+        public PointF UnitPosition { get; set; }
+        public PointF LocationPosition { get; set; }
+        public PointF BarcodeTextPosition { get; set; }
+        public PointF NotePosition { get; set; }
+
+        public LabelViewModel()
+        {
+            // Default constructor
+        }
+
+        public void GenerateLabel(string filePath, string barcodeStr)
+        {
+            // Create a new document
+            Document document = new(new iTextSharp.text.Rectangle(208f, 148f), 0, 0, -5, 0);
+            PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+
+            document.Open();
+
+            // Add barcode
+            BarcodeWriter writerEan = new()
+            {
+                Format = BarcodeFormat.EAN_13,
+                Options = new EncodingOptions
+                {
+                    Width = 200,
+                    Height = 30,
+                    NoPadding = true,
+                    PureBarcode = true
+                }
+            };
+
+            // Add product information
+            iTextSharp.text.Font productNumberFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 25, BaseColor.BLACK);
+            iTextSharp.text.Font productNameFont = FontFactory.GetFont(FontFactory.HELVETICA, 12, BaseColor.BLACK);
+            iTextSharp.text.Font productUnitFont = FontFactory.GetFont(FontFactory.HELVETICA, 10, BaseColor.BLACK);
+            iTextSharp.text.Font productLocationFont = FontFactory.GetFont(FontFactory.HELVETICA, 25, BaseColor.BLACK);
+            iTextSharp.text.Font productBarcodeTextFont = FontFactory.GetFont(FontFactory.HELVETICA, 12, BaseColor.BLACK);
+
+            document.Add(new Paragraph(ProductNumber, productNumberFont) { SpacingBefore = ProductNumberPosition.Y, IndentationLeft = ProductNumberPosition.X });
+            document.Add(new Paragraph(Unit, productUnitFont) { SpacingBefore = UnitPosition.Y, IndentationLeft = UnitPosition.X });
+            document.Add(new Paragraph(ProductName, productNameFont) { SpacingBefore = ProductNamePosition.Y, IndentationLeft = ProductNamePosition.X });
+            document.Add(new Paragraph(Location, productLocationFont) { SpacingBefore = LocationPosition.Y, IndentationLeft = LocationPosition.X });
+
+            var barcodeImage = writerEan.Write(barcodeStr);
+            iTextSharp.text.Image barcode = iTextSharp.text.Image.GetInstance(barcodeImage, System.Drawing.Imaging.ImageFormat.Bmp);
+            barcode.SetAbsolutePosition(BarcodePosition.X, BarcodePosition.Y);
+            document.Add(barcode);
+
+            document.Add(new Paragraph(BarcodeText, productBarcodeTextFont) { SpacingBefore = BarcodeTextPosition.Y, IndentationLeft = BarcodeTextPosition.X });
+
+            document.Close();
+        }
+    }
+}
